af8bea03a9cf1a0b6a127faf38015c9b6002a12f
Abdullah Albanyan <abalbanyan@hotmail.com>
2017-03-12 20:00:33 -0700

    updated tutorial

--- Globals.js
+++ Globals.js
@@ -6,4 +6,25 @@ var fadeToBlack = false;
 var spooky = false;
 var testKeys = 0;
 var gameStart = 0;
-var gameInstructions = `Placeholder. Press 'i' to toggle game instructions`;
+var gameInstructions = `
+<center> TUTORIAL </center>
+<br>
+Use <b>WASD</b> or the left joystick to move around.
+<br>
+Use the <b>arrow keys</b> or the right joystick to move the camera.
+<br>
+Press <i><b>space</b></i> or (A) to pick up objects like food, keys, and notes.
+<br>
+Press <b>'+'/'-'</b> on a keyboard to change movement speed. 
+<br>
+Hold (B) if you're using a controller to sprint. 
+<br>
+Hold <i><b>'SHIFT'</b></i> or (X) to crouch down.
+<br> 
+Press the buttons 1 through 6 to open notes you've found. 
+<br>
+Press <b>'i'</b> to close (and re-open!) this tutorial.
+<br>
+Now that you're ready, try picking up the note on the desk.
+<br>
+`;
